4464e82a2fbda0a0539f069a248974cdbdb99256
dreamcat <solitudesword@gmail.com>
2017-05-04 16:50:16 +0800

    004的代码

--- leetcode/algorithms/004_test.go
+++ leetcode/algorithms/004_test.go
@@ -1,6 +1,8 @@
 package algorithms
 
-import "testing"
+import (
+	"testing"
+)
 
 /**
 https://leetcode.com/problems/median-of-two-sorted-arrays/#/description
@@ -24,91 +26,52 @@ The median is (2 + 3)/2 = 2.5
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	count1, count2 := len(nums1), len(nums2)
-	count := count1 + count2
-	midIdx := count / 2
-	start1, start2, end1, end2 := 0, 0, count1-1, count2-1
-	mid := -1
-	var midNum float64
-	for start1 <= end1 {
-		mid1 := (start1 + end1) / 2
-		inAry2 := locaIntInAry(nums2, nums1[mid1], start2, end2)
-		mid = mid1 + inAry2 + 1
-		if mid == midIdx {
-			midNum = float64(nums1[mid1])
-			break
-		} else if mid < midIdx {
-			start1 = mid1 + 1
-			start2 = inAry2
-		} else {
-			end1 = mid1 - 1
-			end2 = inAry2
-		}
+	if count1 > count2 {
+		nums1, nums2, count1, count2 = nums2, nums1, count2, count1
 	}
-	if mid != midIdx {
-		for start2 <= end2 {
-			mid1 := (start2 + end2) / 2
-			inAry2 := locaIntInAry(nums1, nums2[mid1], start1, end1)
-			mid = mid1 + inAry2 + 1
-			if mid == midIdx {
-				midNum = float64(nums2[mid1])
-				break
-			} else if mid < midIdx {
-				start1 = mid1 + 1
-				start2 = inAry2
-			} else {
-				end1 = mid1 - 1
-				end2 = inAry2
-			}
-		}
+	if count2 == 0 {
+		return 0
 	}
-	return midNum
-}
+	imin, imax, halfLen := 0, count1, (count1+count2+1)/2
 
-/**
-在start和end这2个下标之间，存在m个小于find的值，k个等于find的值，返回值n>=m+start且n<=m+k+start
- */
-func locaIntInAry(nums []int, find, start, end int) int {
-	for end-start > 1 {
-		mid := (start + end) / 2
-		if nums[mid] == find {
-			return mid
-		} else if nums[mid] > find {
-			end = mid - 1
+	for imin <= imax {
+		i := (imin + imax) / 2
+		j := halfLen - i
+		if i < count1 && nums2[j-1] > nums1[i] {
+			imin = i + 1
+		} else if i > 0 && nums1[i-1] > nums2[j] {
+			imax = i - 1
 		} else {
-			start = mid + 1
+			var maxLeft int
+			if i == 0 {
+				maxLeft = nums2[j-1]
+			} else if j == 0 {
+				maxLeft = nums1[i-1]
+			} else if nums1[i-1] > nums2[j-1] {
+				maxLeft = nums1[i-1]
+			} else {
+				maxLeft = nums2[j-1]
+			}
+			if (count1+count2)%2 == 1 {
+				return float64(maxLeft)
+			}
+			var minRight int
+			if i == count1 {
+				minRight = nums2[j]
+			} else if j == count2 {
+				minRight = nums1[i]
+			} else if nums1[i] < nums2[j] {
+				minRight = nums1[i]
+			} else {
+				minRight = nums2[j]
+			}
+			return float64(maxLeft+minRight) / 2.0
 		}
 	}
-	if start == end {
-		if nums[start] < find {
-			return start + 1
-		} else {
-			return start
-		}
-	} else if nums[start] >= find {
-		return start
-	} else if nums[end] >= find {
-		return end
-	} else {
-		return end + 1
-	}
+	return 0
 }
 
 func TestFindMedianSortedArrays(t *testing.T) {
-	type tmpS struct {
-		ary  []int
-		find int
-	}
-	list := []tmpS{
-		{[]int{1, 3, 4, 8, 9}, 5},
-		{[]int{1, 3, 4, 5, 5, 5, 5, 5, 5, 8, 9}, 5},
-	}
-	for _, tmp := range list {
-		n := locaIntInAry(tmp.ary, tmp.find, 0, len(tmp.ary)-1)
-		t.Logf("input : %s, find : %d, output : %d\n", formatIntAry(tmp.ary), tmp.find, n)
-	}
-
-	// return
-
 	type TestCases struct {
 		nums1  []int
 		nums2  []int
